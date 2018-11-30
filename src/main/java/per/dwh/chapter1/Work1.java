61d98a9b69b3f2492bb1d4b159a7facc6a951702
D-W-X-H <171250507@smail.nju.edu.cn>
2018-11-30 10:56:24 +0800

    rearranged.

---
+++ src/main/java/per/dwh/chapter1/Work1.java
@@ -0,0 +1,111 @@
+package per.dwh.chapter1;
+
+import java.util.Scanner;
+
+/**
+ * @author DW
+ * @date 2018/11/29
+ */
+
+class Work1_3 {
+    static void compareThree() {
+        //read 3 ints
+        Scanner s = new Scanner(System.in);
+        int number = 3;
+        int[] a = new int[number];
+        for (int i = 0; i < number; i++) {
+            a[i] = s.nextInt();
+        }
+
+        //compare
+        if ((a[0] ^ a[1]) != 0 || (a[2] ^ a[1]) != 0) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
+    }
+}
+
+class Work1_5 {
+    static boolean testDouble(double a, double b) {
+        return (a > 0) && (a < 1) && (b > 0) && (b < 1);
+    }
+}
+
+class Work1_9 {
+    static String getBinary(int number) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (number == 0) {
+            return "0";
+        }
+        while (number != 0) {
+            if (number % 2 == 0) {
+                stringBuilder.append("0");
+                number = number / 2;
+            } else {
+                stringBuilder.append("1");
+                number = number / 2;
+            }
+        }
+        return stringBuilder.reverse().toString();
+    }
+
+    static String answer(int number) {
+        StringBuilder s = new StringBuilder();
+        for (int n = number; n > 0; n /= 2) {
+            s.insert(0, (n % 2));
+        }
+        return s.toString();
+    }
+}
+
+class Work1_11 {
+    private static final String SPACE = "   ";
+
+    static void printMatrix(boolean[][] b) {
+        StringBuilder s = new StringBuilder();
+        s.append("    ");
+        for (int i = 1; i <= b[0].length; i++) {
+            addNumber(s, i);
+        }
+        s.append("\n");
+
+        for (int i = 1; i <= b.length; i++) {
+            addNumber(s, i);
+            for (int j = 0; j < b[i - 1].length; j++) {
+                if (b[i - 1][j]) {
+                    s.append("*").append(SPACE);
+                } else {
+                    s.append(" ").append(SPACE);
+                }
+            }
+            s.append("\n");
+        }
+        System.out.println(s);
+    }
+
+    private static void addNumber(StringBuilder s, int number) {
+        if (number < 10) {
+            s.append(number).append("   ");
+        } else if (number < 100) {
+            s.append(number).append("  ");
+        } else if (number < 1000) {
+            s.append(number).append(" ");
+        }
+    }
+}
+
+class Work1_13 {
+    static int[][] reverse(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return null;
+        }
+        int[][] newMatrix = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                newMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return newMatrix;
+    }
+}
---
+++ src/test/java/per/dwh/chapter1/Test1.java
@@ -0,0 +1,223 @@
+package per.dwh.chapter1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author DW
+ * @date 2018/11/29
+ */
+class Test1_3 {
+    private ByteArrayOutputStream bytes = null;//输出流
+    private ByteArrayInputStream inputStream = null;//输入流
+    private String numbers = null;//被测试数据
+    private final String lineSeparator = System.lineSeparator();
+
+    //重定向输出流
+    @BeforeEach
+    void setUp() {
+        bytes = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bytes));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+    }
+
+    //测试
+    @Test
+    void test1() {
+        numbers = "12" + lineSeparator + "12" + lineSeparator + "12" + lineSeparator;
+        inputStream = new ByteArrayInputStream(numbers.getBytes());
+        System.setIn(inputStream);
+        Work1_3.compareThree();
+        assertEquals("true", bytes.toString().trim());
+    }
+
+    @Test
+    void test2() {
+        numbers = "12" + lineSeparator + "14" + lineSeparator + "12" + lineSeparator;
+        inputStream = new ByteArrayInputStream(numbers.getBytes());
+        System.setIn(inputStream);
+        Work1_3.compareThree();
+        assertEquals("false", bytes.toString().trim());
+    }
+
+    @Test
+    void test3() {
+        numbers = "12" + lineSeparator + "12" + lineSeparator + "14" + lineSeparator;
+        inputStream = new ByteArrayInputStream(numbers.getBytes());
+        System.setIn(inputStream);
+        Work1_3.compareThree();
+        assertEquals("false", bytes.toString().trim());
+    }
+
+    @Test
+    void test4() {
+        numbers = "11" + lineSeparator + "14" + lineSeparator + "14" + lineSeparator;
+        inputStream = new ByteArrayInputStream(numbers.getBytes());
+        System.setIn(inputStream);
+        Work1_3.compareThree();
+        assertEquals("false", bytes.toString().trim());
+    }
+
+    @Test
+    void test5() {
+        numbers = "10" + lineSeparator + "11" + lineSeparator + "14" + lineSeparator;
+        inputStream = new ByteArrayInputStream(numbers.getBytes());
+        System.setIn(inputStream);
+        Work1_3.compareThree();
+        assertEquals("false", bytes.toString().trim());
+    }
+}
+
+class Test1_5 {
+    @Test
+    void test1() {
+        assertTrue(Work1_5.testDouble(0.1, 0.1));
+    }
+
+    @Test
+    void test2() {
+        assertFalse(Work1_5.testDouble(0, 0.1));
+    }
+
+    @Test
+    void test3() {
+        assertFalse(Work1_5.testDouble(0.1, 0));
+    }
+
+    @Test
+    void test4() {
+        assertFalse(Work1_5.testDouble(-0.1, -0.1));
+    }
+
+    @Test
+    void test5() {
+        assertFalse(Work1_5.testDouble(1, 0.1));
+    }
+
+    @Test
+    void test6() {
+        assertFalse(Work1_5.testDouble(0.34, 1));
+    }
+}
+
+class Test1_9 {
+    @Test
+    void test1() {
+        Random random = new Random(System.currentTimeMillis());
+        for (int i = 0; i < 1000; i++) {
+            int a = random.nextInt();
+            a = Math.abs(a);
+            assertEquals(Integer.toBinaryString(a), Work1_9.getBinary(a), Integer.toString(a));
+        }
+    }
+}
+
+class Test1_11 {
+    private Random random = new Random(System.currentTimeMillis());
+
+    @Test
+    void test1() {
+        boolean[][] matrix = new boolean[4][];
+        matrix[0] = new boolean[]{false, true, true, false};
+        matrix[1] = new boolean[]{false, false, false, true};
+        matrix[2] = new boolean[]{false, false, true, false};
+        matrix[3] = new boolean[]{true, true, true, false};
+        String sTrue = "*   ";
+        String sFalse = "    ";
+        String s = "    1   2   3   4   \n" +
+                "1   " + sFalse + sTrue + sTrue + sFalse + "\n" +
+                "2   " + sFalse + sFalse + sFalse + sTrue + "\n" +
+                "3   " + sFalse + sFalse + sTrue + sFalse + "\n" +
+                "4   " + sTrue + sTrue + sTrue + sFalse + "\n";
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bytes));
+        Work1_11.printMatrix(matrix);
+        assertEquals(s.trim(), bytes.toString().trim());
+    }
+
+    //I don't want to write more test methods because it maybe too long and it's boring.
+    @Test
+    void test2() {
+        int a = 0, b = 0;
+        while (a == 0) {
+            a = random.nextInt(100);
+        }
+        while (b == 0) {
+            b = random.nextInt(100);
+        }
+        boolean[][] matrix = new boolean[a][b];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = random.nextBoolean();
+            }
+        }
+        System.err.println(matrix.length + "  " + matrix[0].length);
+
+        Work1_11.printMatrix(matrix);
+    }
+}
+
+class Test1_13 {
+    private int[][] matrix;
+    private int[][] outMatrix;
+
+    @Test
+    void test1() {
+        matrix = new int[4][];
+        matrix[0] = new int[]{1, 3, 5, 7, 3};
+        matrix[1] = new int[]{3, 4, 6, 8, 1};
+        matrix[2] = new int[]{12, 45, 2, 6, 9};
+        matrix[3] = new int[]{6, 3, 7, 2, 9};
+        outMatrix = new int[5][];
+        outMatrix[0] = new int[]{1, 3, 12, 6};
+        outMatrix[1] = new int[]{3, 4, 45, 3};
+        outMatrix[2] = new int[]{5, 6, 2, 7};
+        outMatrix[3] = new int[]{7, 8, 6, 2};
+        outMatrix[4] = new int[]{3, 1, 9, 9};
+        assertArrayEquals(outMatrix, Work1_13.reverse(matrix));
+    }
+
+    @Test
+    void test2() {
+        matrix = new int[2][];
+        matrix[0] = new int[]{1, 3, 5, 7, 3};
+        matrix[1] = new int[]{3, 4, 6, 8, 1};
+        outMatrix = new int[5][];
+        outMatrix[0] = new int[]{1, 3};
+        outMatrix[1] = new int[]{3, 4};
+        outMatrix[2] = new int[]{5, 6};
+        outMatrix[3] = new int[]{7, 8};
+        outMatrix[4] = new int[]{3, 1};
+        assertArrayEquals(outMatrix, Work1_13.reverse(matrix));
+    }
+
+    @Test
+    void test3() {
+        matrix = new int[5][];
+        matrix[0] = new int[]{1, 3, 5, 7, 3};
+        matrix[1] = new int[]{3, 4, 6, 8, 1};
+        matrix[2] = new int[]{12, 45, 2, 6, 9};
+        matrix[3] = new int[]{6, 3, 7, 2, 9};
+        matrix[4] = new int[]{234, 123, 1, 12, 34};
+        outMatrix = new int[5][];
+        outMatrix[0] = new int[]{1, 3, 12, 6, 234};
+        outMatrix[1] = new int[]{3, 4, 45, 3, 123};
+        outMatrix[2] = new int[]{5, 6, 2, 7, 1};
+        outMatrix[3] = new int[]{7, 8, 6, 2, 12};
+        outMatrix[4] = new int[]{3, 1, 9, 9, 34};
+        assertArrayEquals(outMatrix, Work1_13.reverse(matrix));
+    }
+
+}
